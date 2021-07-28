0fb693d943b281e845cdbe2358f374c22d9d8ec6
LubomirP <lubomirpacheliev@gmail.com>
2021-07-28 20:17:57 +0300

    after auth redirect

--- src/app/auth/services/login/login.service.ts
+++ src/app/auth/services/login/login.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(public fireauth: AngularFireAuth) { }
+  constructor(public fireauth: AngularFireAuth, public router: Router) { }
 
   login(user: {email?: string, password?: string, repassword?: string}) {
-    if (user.email && user.password) this.fireauth.signInWithEmailAndPassword(user.email, user.password);
+    if (user.email && user.password) {
+      this.fireauth.signInWithEmailAndPassword(user.email, user.password);
+      this.router.navigate(['/posts']);
+    }
     // TODO: Fail notification
-    // TODO: Navigate to posts page
   }
 }
--- src/app/auth/services/register/register.service.ts
+++ src/app/auth/services/register/register.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-  constructor(public fireauth: AngularFireAuth) {}
+  constructor(public fireauth: AngularFireAuth, public router: Router) {}
 
   register(user: {email?: string, password?: string, repassword?: string}) {
     if (user.password === user.repassword) {
-      if (user.email && user.password) this.fireauth.createUserWithEmailAndPassword(user.email, user.password);
+      if (user.email && user.password) {
+        this.fireauth.createUserWithEmailAndPassword(user.email, user.password);
+        this.router.navigate(['posts']);
+      }
     } else {
       // TODO: Add notification or something
     }
