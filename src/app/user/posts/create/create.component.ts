c96bfa06528c7d36bacd4abe0b775cfeaa785488
LubomirP <lubomirpacheliev@gmail.com>
2021-07-31 13:38:14 +0300

    Creating posts

--- src/app/user/posts/create/create.component.ts
+++ src/app/user/posts/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { CreatePostService } from '../../services/create-post/create-post.service';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +10,7 @@ import { Component } from '@angular/core';
 })
 export class CreateComponent  {
 
-  constructor() { }
+  constructor(public createService: CreatePostService, public fbAuth: AngularFireAuth, public router: Router) { }
 
   createPost() {
     const [title, content]: (HTMLInputElement | HTMLTextAreaElement | null)[] = [
@@ -15,9 +18,19 @@ export class CreateComponent  {
       document.querySelector('#content')
     ];
 
-    const post = {
-      title: title?.value,
-      content: content?.value
-    }
+    let user: string | null;
+
+    this.fbAuth.user.subscribe(userData => {
+      user = userData!.email;
+      const post = {
+        title: title?.value,
+        content: content?.value,
+        user: user
+      }
+  
+      this.createService.create(post);
+    });
+
+    this.router.navigate(['posts']);
   }
 }
--- src/app/user/services/create-post/create-post.service.ts
+++ src/app/user/services/create-post/create-post.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class CreatePostService {
 
-  constructor() { }
+  constructor(public firestore: AngularFirestore) { }
+
+  create(post: { title?: string, content?: string, user?: string | null}) {
+    this.firestore.collection('posts').add(post);
+  }
 }
