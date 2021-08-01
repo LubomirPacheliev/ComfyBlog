db031e3b1b85e6e25e81ed818c58ef677170cf2a
LubomirP <lubomirpacheliev@gmail.com>
2021-08-01 04:53:09 +0300

    update operation

--- src/app/user/posts/postedit/postedit.component.ts
+++ src/app/user/posts/postedit/postedit.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { PostEditService } from '../../services/post-edit/post-edit.service';
 
 @Component({
   selector: 'app-postedit',
   templateUrl: './postedit.component.html',
   styleUrls: ['./postedit.component.css']
 })
-export class PosteditComponent implements OnInit {
+export class PosteditComponent {
 
-  constructor() { }
+  constructor(public firestore: AngularFirestore, public route: ActivatedRoute, public editPost: PostEditService, public fireatuh: AngularFireAuth) { }
 
-  ngOnInit(): void {
-  }
+  edit() {
+    let id: string; 
+    let data: {};
+    const [title, content]: (HTMLInputElement | null)[] = [
+      document.querySelector('#title'),
+      document.querySelector('#content')
+    ];
 
-  editPost() {
-    
+    this.route.params.subscribe(param => {
+      if (title?.value || content?.value) {
+        content?.value 
+        ? data = {title: title?.value, content: content.value}
+        : data = {title: title?.value};
+        id = param.id;
+        this.editPost.update(id, data);
+      }
+    });
   }
 
 }
--- src/app/user/services/post-edit/post-edit.service.ts
+++ src/app/user/services/post-edit/post-edit.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostEditService {
 
-  constructor() { }
+  constructor(public firestore: AngularFirestore, public router: Router) { }
+
+  update(id: string, changes: {}) {
+    this.firestore.collection('posts').doc(id).update(changes).then(() => {
+      this.router.navigate([`posts/${id}`]);
+    });
+  }
 }
