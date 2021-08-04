17cc4c84bf9021abf3eff1ff5b5c56e7adb9ff75
LubomirP <lubomirpacheliev@gmail.com>
2021-08-04 11:10:48 +0300

    checking if the user is authenticated

--- src/app/user/guards/can-activate.guard.ts
+++ src/app/user/guards/can-activate.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -6,11 +7,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CanActivateGuard implements CanActivate {
+  constructor(public auth: AngularFireAuth) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      return this.auth.currentUser.then(res => {
+        if (res?.email) {
+          return true;
+        } else {
+          return false;
+        }
+      });
   }
   
 }
